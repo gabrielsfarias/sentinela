5a70f711f9ada50e3da0415b14bb0e5969e66d10
Gabriel Souza Farias <gabrielsfarias@outlook.com>
2025-04-13 00:41:38 -0300

    feat(api): adiciona repositórios e serviços ao contêiner de injeção de dependência

--- src/SentinelaDocumentos.Api/Program.cs
+++ src/SentinelaDocumentos.Api/Program.cs
@@ -1,9 +1,19 @@
+using SentinelaDocumentos.Application.Interfaces;
+using SentinelaDocumentos.Application.Services;
+using SentinelaDocumentos.Domain.Interfaces;
+using SentinelaDocumentos.Infrastructure.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITipoDocumentoRepository, EfTipoDocumentoRepository>();
+builder.Services.AddScoped<IDocumentoEmpresaRepository, EfDocumentoEmpresaRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IDocumentoAppService, DocumentoAppService>();
 
 var app = builder.Build();
 
@@ -16,29 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// Configurando os endpoints principais da API
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
